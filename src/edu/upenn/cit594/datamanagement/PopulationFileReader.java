2119aabec3a9f5d140d075f44272954b96f2452c
Tim Wang <wzh1022@seas.upenn.edu>
2023-08-05 01:38:47 -0400

    Created PopulationFileReader and PropertyFileReader class

---
+++ src/edu/upenn/cit594/datamanagement/PopulationFileReader.java
@@ -0,0 +1,20 @@
+package edu.upenn.cit594.datamanagement;
+
+import java.util.List;
+
+import edu.upenn.cit594.util.Population;
+public class PopulationFileReader {
+	private String fileName;
+	
+	public PopulationFileReader(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	/**
+	 * Read population data files
+	 * @return list of population data
+	 */
+	public List<Population> readPopulation() {
+		return null;
+	}
+}
---
+++ src/edu/upenn/cit594/datamanagement/PropertyFileReader.java
@@ -0,0 +1,23 @@
+package edu.upenn.cit594.datamanagement;
+
+import java.util.List;
+
+import edu.upenn.cit594.util.Property;
+
+public class PropertyFileReader {
+
+	private String fileName;
+	
+	public PropertyFileReader(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	/**
+	 * Read property data
+	 * @return list of properties
+	 */
+	public List<Property> readProperty(){
+		return null;
+	}
+	
+}
